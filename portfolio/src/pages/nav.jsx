e150c5e292e65c5f032526e29ad6a48f1b06573b
amoako419 <ghheskey@gmail.com>
2024-04-15 23:59:09 +0000

    initial commit

--- portfolio/src/pages/nav.jsx
+++ portfolio/src/pages/nav.jsx
@@ -3,11 +3,21 @@ import '../App.css';
 
 function Nav(){
     return(
-        <div className="logo">
-        <img src="me.jpg" alt="mypicture" />
-       <p><span className="name">Amoako Heskey</span> 
-        <br />
-        Software Engineer (ML)</p>
+        <div className="nav">
+             <div className="logo">
+            <img src="me.jpg" alt="mypicture" />
+            <p><span className="name">Amoako Heskey</span> 
+             <br />
+            Software Engineer (ML)</p>
+              </div>
+        <div className="list">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Research</li>
+                <li>Projects</li>
+            </ul>
+        </div>
         </div>
     );
 }
