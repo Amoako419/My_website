39e152ff78fff702021c1a2fa69283bfd27e6c6f
amoako419 <ghheskey@gmail.com>
2024-06-11 21:31:21 +0000

    shit works fam

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 // import About from './pages/about';
 import Home from './pages/home';
 // import Projects from './pages/projects';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <header className="App-header">
     <Home />
    
+    <div id="detail">
+    <Outlet />
+  </div>
     </header>
   );
 }
--- portfolio/src/pages/nav.jsx
+++ portfolio/src/pages/nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import '../App.css';
 import '../App.js';
 import '../index.js';
+import { Outlet, Link } from "react-router-dom";
 // import About from './pages/about';
 // import Home from './pages/home';
 // import Projects from './pages/projects';
@@ -19,12 +20,16 @@ function Nav(){
               </div>
         <div className="list">
             <ul>
-               {/* <Link to ="/"> <li>Home</li></Link>
-               <Link to ="/about"> <li>About</li></Link>
-               < to ="/projects"> <li>Projects</li></> */}
-               <a href="home.jsx"><li>Home</li></a>
-               <a href="about.jsx"><li>About</li></a>
-               <a href="projects.jsx"><li>Projects</li></a>
+              <li>
+                <Link to={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link to={'/about'}>About</Link>
+              </li>
+              <li>
+                <Link to = {'/projects'}>Projects</Link>
+              </li>
+
                
                 {/* <li>Research</li> */}
                 {/* <li>Projects</li> */}
