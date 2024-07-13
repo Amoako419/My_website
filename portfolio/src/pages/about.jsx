98f2edfe68ea1fffba14856526aedb4f0984b10b
amoako419 <ghheskey@gmail.com>
2024-07-13 01:38:46 +0000

    ui updated

--- portfolio/src/pages/about.jsx
+++ portfolio/src/pages/about.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Nav from "./nav";
+import '../App.css';
+import '../App.js';
+import '../index.js';
 
 
 function About(){
     return(
+        
         <div className="about">
+        <Nav />
         <h2>This is an about page</h2>
         </div>
     );
--- portfolio/src/pages/projects.jsx
+++ portfolio/src/pages/projects.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Nav from "./nav";
+import '../App.css';
+import '../App.js';
+import '../index.js';
 
 
 function Projects(){
     return(
-        <div className="about">
+        <div className="projects">
+        <Nav />
         <h2>This is an project page</h2>
         </div>
     );
